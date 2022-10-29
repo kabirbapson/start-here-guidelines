516ae88da0a94bd81c010aa3038ab443afc3aa99
Bappi Kabir <kabirbapson@gmail.com>
2022-10-29 01:41:24 +0100

    listtest1

--- sqrt.js
+++ sqrt.js
@@ -5,10 +5,10 @@ window.addEventListener("DOMContentLoaded", () => {
     if (!ans || isNaN(ans) || ans < 0) {
       throw new Error("Not a valid Number");
     } else if (ans == 0) {
-      alert("we dont root ", ans);
+      alert("we don\'t root ", ans);
       
     } else {
-        alert("The square root of" + ans + " is " + Math.sqrt(ans).toFixed(2));
+        alert("The square root of " + ans + " is " + Math.sqrt(ans).toFixed(2));
     }
   } catch (err) {
     alert(err.message);
